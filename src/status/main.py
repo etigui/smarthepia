69bd9095578931162eb2504005fcb80a42cb771a
raccoonmaster <32940684+raccoonmaster@users.noreply.github.com>
2018-05-13 22:40:30 +0200

    New version zwave

--- src/status/main.py
+++ src/status/main.py
@@ -9,26 +9,3 @@ if __name__ == "__main__":
     main()
 
 
-'''
-## Compare DB(info) & REST(info)
-
-{   battery: 100,
-    controller: "Pi 1", "Pi 2", "Pi 3"
-    humidity: 29,
-    location: "A505",
-    luminance: 6,
-    motion: false,
-    sensor: 3,
-    temperature: 26.8,
-    updateTime: 1526152996}
-    
-    
-## Check battery
-    if(battery <= 20%)
-        print(bettry low)
-        
-## Check uptime
-    if(updateTime < now - 1J)
-        print(uptime error) => the sensor dont send (give) data anymore
-    
-'''
--- src/status/rpiinfo.py
+++
@@ -1,8 +0,0 @@
-class RPIInfo():
-    def __init__(self, id, ip, port, sensor_ids):
-        self.id = id
-        self.ip = ip
-        self.port = port
-
-        # {'sensor_id': ['Multi Sensor/empty => (1/0)', 'Json => (sensor ok)/HTML => (Sensor error)']}
-        self.sensor_ids = sensor_ids
