6a15f5d0c493caff95d39c8f9e0498d455fc9319
raccoonmaster <32940684+raccoonmaster@users.noreply.github.com>
2018-05-15 00:29:27 +0200

    Modify value to dico

--- src/status/zwave.py
+++ src/status/zwave.py
@@ -47,31 +47,32 @@ def check_zwave_network():
 
         # Check RPI (ping)
         if utils.send_ping(db_rpi.ip):
-            status.setdefault(db_rpi.rpi, []).append([0, db_rpi.rpi,"RPI available"])
+            status.setdefault(db_rpi.rpi, []).append({"status": 0, "path":db_rpi.rpi, "reason": "RPI available"})
 
             # Check REST server (ping)
             if utils.rest_server_status(db_rpi.ip + ":" + db_rpi.port):
-                status.setdefault(db_rpi.rpi, []).append([0, db_rpi.rpi,"REST server available"])
+                status.setdefault(db_rpi.rpi, []).append({"status": 0, "path":db_rpi.rpi, "reason": "REST server available"})
+
 
                 # Get sensor list from REST server => http://ip:port/nodes/get_nodes_list
                 result, rest_sensors = get_sensors_list(db_rpi.ip, db_rpi.port)
                 if result:
-                    status.setdefault(db_rpi.rpi, []).append([0, db_rpi.rpi, "Route get_nodes_list available"])
+                    status.setdefault(db_rpi.rpi, []).append({"status": 0, "path":db_rpi.rpi, "reason": "Route get_nodes_list available"})
 
                     # db_sensor => (location[0], sensor_id[1], status[2])
                     for db_sensor in db_rpi.sensors:
 
                         # Merging between RPI name and Sensor id (rpi1.S1 => Raspberry PI1.Sensor1)
-                        error_device = f'{db_rpi.rpi}.S{db_sensor[1]}'
+                        error_device = f"{db_rpi.rpi}.S{db_sensor['sensor_id']}"
 
                         # If status != 0 => sensor is disabled (no need to check it)
-                        if db_sensor[2] == '0':
+                        if db_sensor['status'] == '0':
 
                             # Check if the sensor id is available in the rest server (compare with db)
-                            if db_sensor[1] in rest_sensors:
+                            if db_sensor['sensor_id'] in rest_sensors:
 
                                 # Get info (REST) from current sensor id => http://ip:port/sensors/sensor_id/get_all_measures
-                                result, rest_info = check_sensor_availability(db_sensor[1], db_rpi.ip, db_rpi.port)
+                                result, rest_info = check_sensor_availability(db_sensor['sensor_id'], db_rpi.ip, db_rpi.port)
                                 if result:
 
                                     # Check if return JSON with sensor info or if the sensor is not available
@@ -82,34 +83,34 @@ def check_zwave_network():
 
                                         # Check if sensor id from RPI is the same as the sensor id from REST server
                                         # Compare room (A500 => A500) and RPI name (rpi1 => PI 1)
-                                        if (rest_info['controller'].replace("Pi ", "rpi") == db_rpi.rpi) and (rest_info['location'] == db_sensor[0]):
+                                        if (rest_info['controller'].replace("Pi ", "rpi") == db_rpi.rpi) and (rest_info['location'] == db_sensor['location']):
                                             # status.setdefault(db_rpi.rpi, []).append([0, "Sensor id from REST == sensor id from RPI DB"])
                                             # TODO check timestamp updatetime/battery level
                                             pass
                                         else:
-                                            status.setdefault(db_rpi.rpi, []).append([1, error_device, "Sensor id from REST server not match with the RPI DB"])
+                                            status.setdefault(db_rpi.rpi, []).append({"status": 1, "path": error_device, "reason": "Sensor id from REST server not match with the RPI DB"})
                                     elif rest_info.find('Node not ready or wrong sensor node type') != -1:
-                                        status.setdefault(db_rpi.rpi, []).append([1, error_device, "Sonsor not available"])
+                                        status.setdefault(db_rpi.rpi, []).append({"status": 1, "path": error_device, "reason": "Sonsor not available"})
                                     else:
-                                        status.setdefault(db_rpi.rpi, []).append([1, error_device, "Sonsor error or not available"])
+                                        status.setdefault(db_rpi.rpi, []).append({"status": 1, "path": error_device, "reason": "Sonsor error or not available"})
                                 else:
-                                    status.setdefault(db_rpi.rpi, []).append([1, error_device, "Route get_all_measures not available"])
+                                    status.setdefault(db_rpi.rpi, []).append({"status": 1, "path": error_device, "reason": "Route get_all_measures not available"})
                             else:
-                                status.setdefault(db_rpi.rpi, []).append([1, error_device, "Sensor id not found (REST server)"])
+                                status.setdefault(db_rpi.rpi, []).append({"status": 1, "path": error_device, "reason": "Sensor id not found (REST server)"})
                 else:
-                    status.setdefault(db_rpi.rpi, []).append([1, db_rpi.rpi, "Route get_nodes_list not available"])
+                    status.setdefault(db_rpi.rpi, []).append({"status": 1, "path": db_rpi.rp, "reason": "Route get_nodes_list not available"})
             else:
-                status.setdefault(db_rpi.rpi, []).append([1, db_rpi.rpi, "REST server not available"])
+                status.setdefault(db_rpi.rpi, []).append({"status": 1, "path": db_rpi.rp, "reason": "REST server not available"})
         else:
-            status.setdefault(db_rpi.rpi, []).append([1, db_rpi.rpi, "RPI not available"])
+            status.setdefault(db_rpi.rpi, []).append({"status": 1, "path": db_rpi.rp, "reason": "RPI not available"})
 
 
     # Retrive errors
     for key, values in status.items():
         #print(f'Status for {key}: {values}')
-        print(f'RPI: {key}')
+        print(f"RPI: {key}")
         for value in values:
-            print(f'{value}')
+                print(f"[{value['status']}] [{value['path']}] [{value['reason']}]")
         print('')
 
 
@@ -119,12 +120,13 @@ def get_db_sensors(mdb_host):
     db_sensors = []
     tmp = {}
     client = pymongo.MongoClient(mdb_host)
-    sensors = client.smarthepia.multisensors.find({}, {'_id': False, 'rpi': True, 'ip': True, 'port': True, 'location': True, 'sensor_id': True, 'status': True})
+    sensors = client.smarthepia.multisensors.find({}, {'_id': False, 'rpi': True, 'ip': True, 'port': True, 'location': True, 'sensor_id': True, 'status': True, 'type': True})
 
     # Add sensor in dico with key (rpi,ip,port) and list of sensor id
     for sensor in sensors:
         key = f"{sensor['rpi']};{sensor['ip']};{sensor['port']}"
-        values = [sensor['location'],sensor['sensor_id'],sensor['status']]
+        #values = [sensor['location'], sensor['sensor_id'], sensor['status'], sensor['type']]
+        values = {"location": sensor['location'], "sensor_id": sensor['sensor_id'], "status": sensor['status']}
         tmp.setdefault(key, []).append(values)
 
     # Add all sensors in struct
