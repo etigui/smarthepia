97d1f0766083ce1c1be96f696941e490683bf006
raccoonmaster <32940684+raccoonmaster@users.noreply.github.com>
2018-06-10 21:20:17 +0200

    Minor modif socket

--- src/web/sh/sockets/socket.js
+++ src/web/sh/sockets/socket.js
@@ -1,47 +1,49 @@
 'use strict';
 
 // Module Dependencies
-var UserModel = require('../models/user');
+var User = require('../models/user');
 var io = require('socket.io')();
 
 // Module variables
 var userSockets = {};
 
-// Config socket connection
+// Client connected
 io.on('connection', function (socket) {
     console.log('A client has connected');
-    console.log('the socket session object', socket.request.session);
+    //console.log('the socket session object', socket.request.session);
     console.log('the actual serialized user from passport', socket.request.session.passport.user);
-    //store '_id' of connected user in order to access it easily
+
+    // Get and store _id of connected user in order to access it easily
+    // from other modules e.g. from router
     var ID = socket.request.session.passport.user;
-    var userPermission = -1;
-    //store actual socket of connected user in order to access it easily
-    //from other modules e.g. from router
     userSockets[ID] = socket;
 
-    UserModel.findOne({_id: ID}, function (err, user) {
+    // Get user permission to let only authorized user to talk (even normally the other can't)
+    var userPermission = -1;
+
+    // Check if current user exist and get user info
+    // passport id most match with => _id
+    User.findOne({_id: ID}, function (err, user) {
         if(err) {
             console.error(err);
             throw(err);
         }
-        var data = {
-            firstName: user.firstName,
-            lastName: user.lastName
-        };
         userPermission = user.permissions;
-        console.log('sending additional data to dashboard', data);
-        return socket.emit('welcome', data);
+        return socket.emit('welcom', "coucou");
     });
-    //on disconnect
+
+    // Client disconnected
     socket.on('disconnect', function () {
         delete userSockets[ID];
         return console.log('The client has disconnected');
     });
 
+    // Received from client (message)
     socket.on('message', function (data) {
 
         // Only admin cas send message
         if(userPermission > 0) {
+            console.log(data);
             socket.broadcast.emit('alarmModified', data);
         }
     });
