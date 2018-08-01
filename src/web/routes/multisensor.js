23e66babc5ca184dce05ff5bc9926de862a9f1ff
raccoonmaster <32940684+raccoonmaster@users.noreply.github.com>
2018-08-01 09:52:46 +0200

    add getUser perm root ms

--- src/web/routes/multisensor.js
+++ src/web/routes/multisensor.js
@@ -17,7 +17,7 @@ router.use(passport.session());
 
 // GET /test
 router.get('/',  isAuth, function(req, res, next) {
-    if(auth.checkPermission(req, auth.getManager())){
+    if(auth.checkPermission(req, auth.getUser())){
         return res.render('pages/multisensor', { lastname: req.session.lastname, dateTime: dateFormat(new Date(), "HH:MM:ss mm/dd/yyyy"),permission: req.session.permissions, page: "multisensor"});
     }else{
         return res.redirect('/');
